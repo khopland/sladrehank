cd1faa10d014a0abfb8dd72facdf9f2ce04e4971
Kristoffer Larsen Hopland <24739853+khopland@users.noreply.github.com>
2026-01-02 12:22:24 +0100

    impl echo

--- src/bin/echo.rs
+++ src/bin/echo.rs
@@ -1,3 +1,55 @@
-fn main(){
-    println!("Hello, world!");
+use anyhow::Context;
+use maelstrom_echo::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+enum Payload {
+    Echo { echo: String },
+    EchoOk { echo: String },
+}
+struct EchoNode {
+    id: usize,
+}
+impl Node<(), Payload> for EchoNode {
+    fn from_init(
+        _state: (),
+        init: Init,
+        _inject: std::sync::mpsc::Sender<Event<Payload, ()>>,
+    ) -> anyhow::Result<Self>
+    where
+        Self: Sized,
+    {
+        Ok(EchoNode {
+            id: init.node_id.parse().unwrap_or(0),
+        })
+    }
+
+    fn step(
+        &mut self,
+        input: Event<Payload, ()>,
+        output: &mut std::io::StdoutLock,
+    ) -> anyhow::Result<()> {
+        match input {
+            Event::Message(msg) => {
+                let mut reply = msg.into_reply(Some(&mut self.id));
+                match reply.body.payload {
+                    Payload::Echo { echo } => {
+                        reply.body.payload = Payload::EchoOk { echo };
+                        reply.send(output).context("send echo Ok")?;
+                    }
+                    Payload::EchoOk { .. } => {}
+                }
+            }
+            _ => {
+                panic!("unexpected event");
+            }
+        }
+        Ok(())
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    main_loop::<_, EchoNode, _, _>(())
 }
